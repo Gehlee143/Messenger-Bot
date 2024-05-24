49b3245832cdb137116d302916b7a5bdf4d9dca4
Gehlee143 <167980117+Gehlee143@users.noreply.github.com>
2024-05-24 15:18:25 +0800

    sim.js

--- script/sim.js
+++ script/sim.js
@@ -1,31 +1,25 @@
+const name = global.config.BOTNAME;
+const bot = name.toLowerCase();
 module.exports.config = {
-    name: 'sim',
-    version: '1.0.0',
-    role: 0,
-    description: "Engage in conversation with an AI bot",
-    usage: "sim [prompt]",
-    credits: 'Developer',
-    cooldown: 3,
+  name: `${bot}`, 
+  version: "1.0.0",
+  hasPermission: 0,
+  credits: "Prince Sanel Osorio",
+  description: `Chat with ${name} Bot`,
+  commandCategory: "Random",
+  usages: `${global.config.PREFIX}${bot} message`,
+  cooldowns: 0,
 };
 
-module.exports.run = async function({ api, event, args }) {
-    const axios = require("axios");
-    const input = args.join(" ");
-
-    if (!input) {
-        api.sendMessage("Please provide a text prompt. Usage: sim [text]", event.threadID, event.messageID);
-        return;
-    }
-    try {  
-        const content = encodeURIComponent(input);
-        const response = await axios.get(`https://simsimi.fun/api/v2/?mode=talk&lang=ph&message=${content}&filter=false`);
-        const responseData = response.data;
-        if (responseData.error) {
-            api.sendMessage("An error occurred. Please try again later.", event.threadID, event.messageID);
-        } else {
-            api.sendMessage(responseData.success, event.threadID, event.messageID);
-        }
-    } catch (error) {
-        api.sendMessage("An error occurred while fetching the data.", event.threadID, event.messageID);
-    }
-};
+module.exports.run = async function ({ api, event, args }) {
+	var { threadID, messageID } = event;
+	const axios = require("axios");
+	try {
+		const request = args.join(" ");
+		if (!request) return api.sendMessage("[!] Need an message to proceed!", threadID, messageID);
+		const res = await axios.get(`https://mainapi.princemc166.repl.co/api/sim?message=${request}`);
+		api.sendMessage(res.data.message, threadID, messageID);
+	} catch (error) {
+		api.sendMessage("An error occured while fetching the sim api", threadID, messageID);
+	}
+}
